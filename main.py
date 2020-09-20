6c06afb3ab1993a70e42f2d016f955d1addaa630
ErykKnesz <71562823+ErykKnesz@users.noreply.github.com>
2020-09-20 11:01:38 +0200

    fix the new for loop indentation

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ for i in range(1,13):
 
   #another multiplication table, but bigger
   
-  for i in range(1,101):
+for i in range(1,101):
   for j in range(1,101):
     print(i*j, end="\t")
   print('')
+  
 
 
